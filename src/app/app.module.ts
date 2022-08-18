15af3fb958525dd71241af0c5ed49d24a0095b8f
Mihajlo Medjedovic <mihajlo.medjedovic@outlook.com>
2022-08-18 14:22:33 +0200

    feat: clarity removed header, user dropdown and sas logs modal are added as a @sasjs/ngx-sasjs library

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
-
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { FormsModule } from '@angular/forms'
-
-import { LoginModalComponent } from './components/login-modal/login-modal.component'
 import { DataComponent } from './data/data.component'
 import { HomePageComponent } from './home-page/home-page.component'
 import { ClarityModule } from '@clr/angular'
-import { RequestsModalComponent } from './components/requests-modal/requests-modal.component'
 import { UploaderComponent } from './uploader/uploader.component'
+import { NgxSasjsModule } from '@sasjs/ngx-sasjs';
+import { SasjsUiComponent } from './sasjs-ui/sasjs-ui.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginModalComponent,
     DataComponent,
     HomePageComponent,
-    RequestsModalComponent,
-    UploaderComponent
+    UploaderComponent,
+    SasjsUiComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
+    NgxSasjsModule,
     ClarityModule
   ],
   providers: [],
---
+++ src/app/sasjs-ui/sasjs-ui.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SasjsUiComponent } from './sasjs-ui.component';
+
+describe('SasjsUiComponent', () => {
+  let component: SasjsUiComponent;
+  let fixture: ComponentFixture<SasjsUiComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SasjsUiComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(SasjsUiComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
