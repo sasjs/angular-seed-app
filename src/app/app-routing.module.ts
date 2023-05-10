64bed6b146a96c94d34b7f73b8df8585c55a5ba3
Mihajlo Medjedovic <mihajlo.medjedovic@outlook.com>
2023-05-10 15:54:06 +0200

    fix: angular update to 15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,9 +14,8 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      useHash: true,
-      relativeLinkResolution: 'legacy'
-    })
+    useHash: true
+})
   ],
   exports: [RouterModule]
 })
